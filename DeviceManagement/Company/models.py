6b5ebf94767c6a19408f661b5a43e66ceb2510b7
shakirri <ri8shakir@gmail.com>
2024-02-03 20:06:16 +0600

    User Login
    Addition of Authenticated Login Logout Register

--- DeviceManagement/Company/models.py
+++ DeviceManagement/Company/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=20, primary_key=True, serialize=False)
+    
--- DeviceManagement/Company/views.py
+++ DeviceManagement/Company/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import Company
 # Create your views here.
+
+def home(request):
+    return render(request,"Home.html")
+
+def register(request):
+    form=UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username=form.cleaned_data.get("username")
+      messages.success(request, f'your account was created Successfully')
+      a=Company.objects.create(name=username)
+      a.save()
+      return redirect('home')
+    context = {
+        'form':form  
+    } 
+    return render(request, "register.html", context)
+
+@login_required(login_url='login')
+def profile(request):
+    return render(request, 'profile.html')
