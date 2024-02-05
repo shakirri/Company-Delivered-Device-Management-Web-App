0a9ec7b5c04145c549cf0246b6ecfd69795688d7
shakirri <ri8shakir@gmail.com>
2024-02-05 19:37:25 +0600

    Update models.py

--- DeviceManagement/Company/models.py
+++ DeviceManagement/Company/models.py
@@ -3,4 +3,19 @@ from django.db import models
 # Create your models here.
 class Company(models.Model):
     name = models.CharField(max_length=20, primary_key=True, serialize=False)
+
+class Brand(models.Model):
+    brand=models.CharField(max_length=20, primary_key=True, serialize=False)
     
+class Devices(models.Model):
+    deviceID=models.CharField(max_length=20, primary_key=True, serialize=False)
+    deviceName=models.CharField(default=None, blank=True, null=True, max_length=20)
+    type=models.CharField(default=None, blank=True, null=True, max_length=11)
+    brand=models.ForeignKey(Brand)
+    condition=models.CharField(default=None, blank=True, null=True, max_length=11)
+    
+    
+class Employees(models. Model):
+        empID=models.CharField(max_length=20, primary_key=True, serialize=False)
+        name=models.ForeignKey(Company)
+        deviceID=models.ForeignKey(Devices)
